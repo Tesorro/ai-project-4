597809a214729b072c4413d5b2c378ba103182ed
Zaur <z.tsoroev@original-group.ru>
2025-09-18 12:19:57 +0300

    text classification

--- src/components/main-frame.tsx
+++ src/components/main-frame.tsx
@@ -1,5 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from './ui/button';
+import { InferenceClient } from '@huggingface/inference';
+
+const hfClient = new InferenceClient(process.env.NEXT_PUBLIC_HF_TOKEN);
+
+const textToClassify = "I just bought a new camera. It's the best camera I've ever owned!";
+
 interface Props {}
 
 export function MainFrame({}: Props) {
-  return <div>main-frame</div>
+  const [data, setData] = useState<Array<{ label: string; score: number }>>([]);
+  const sendRequest = async () => {
+    const output = await hfClient.textClassification({
+      model: 'tabularisai/multilingual-sentiment-analysis',
+      inputs: textToClassify,
+      provider: 'hf-inference',
+    });
+    if (Array.isArray(output)) {
+      setData(output);
+    }
+  };
+  return (
+    <div>
+      <Button onClick={sendRequest}>SendRequest</Button>
+      <ul>
+        {Array.isArray(data) &&
+          data.length > 0 &&
+          data.map((el, idx) => (
+            <li key={idx}>
+              LABEL: <b>{el.label}</b>; SCORE: <b>{el.score}</b>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
