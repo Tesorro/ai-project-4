ea14096fa9aec8755911090303d2b0856326cfb1
Zaur <z.tsoroev@original-group.ru>
2025-09-18 12:26:45 +0300

    edit main framt

--- src/components/main-frame.tsx
+++ src/components/main-frame.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { InferenceClient } from '@huggingface/inference';
+
+const hfClient = new InferenceClient(process.env.NEXT_PUBLIC_HF_TOKEN);
+
 interface Props {}
 
 export function MainFrame({}: Props) {
