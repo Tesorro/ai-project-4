2f7e70bea5ad5a2005f5bba8bce641766abb5d70
Zaur <z.tsoroev@original-group.ru>
2025-09-18 12:33:53 +0300

    image-to-image

--- src/components/main-frame.tsx
+++ src/components/main-frame.tsx
@@ -1,11 +1,48 @@
 'use client';
 
 import { InferenceClient } from '@huggingface/inference';
+import { useState } from 'react';
+import { Button } from './ui/button';
+import Image from 'next/image';
 
 const hfClient = new InferenceClient(process.env.NEXT_PUBLIC_HF_TOKEN);
 
 interface Props {}
 
 export function MainFrame({}: Props) {
-  return <div>main-frame</div>;
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const sendRequest = async () => {
+    try {
+      const response = await fetch('/photo.avif');
+      if (!response.ok) {
+        throw new Error('Failed to load image!');
+      }
+      const dataBlob = await response.blob();
+      const image = await hfClient.imageToImage({
+        provider: 'fal-ai',
+        model: 'Qwen/Qwen-Image-Edit',
+        inputs: dataBlob,
+        parameters: {
+          prompt: 'Color this photo to make it maximum real as it possible',
+          negative_prompt: 'Black and white photo. text, bad anatomy, blurry, low quality',
+          // // Between 0 and 1 (how close final result could be to original)
+          // strength: 0,
+        },
+      });
+      const imageUrl = URL.createObjectURL(image);
+
+      setImageUrl(imageUrl);
+    } catch (error) {
+      console.log('error loading image: ', error);
+    }
+  };
+  return (
+    <div>
+      <Button onClick={sendRequest}>Send request</Button>
+      <div className="flex gap-10">
+        <Image width={500} height={750} src={'/photo.avif'} alt="image" />
+        {imageUrl && <Image width={500} height={750} src={imageUrl} alt="image" />}
+      </div>
+    </div>
+  );
 }
